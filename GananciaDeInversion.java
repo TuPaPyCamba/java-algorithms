c5914d7e49b9a5b12faf1c72350aa9ade56ae10a
Camba <fercambafcm@gmail.com>
2024-04-05 21:22:17 -0600

    Mejora en calculos de ganancias diarias

--- GananciaDeInversion.java
+++ GananciaDeInversion.java
@@ -1,15 +1,21 @@
 import java.util.Scanner;
 
 // Una persona desea invertir su capital en un banco y desea saber cuánto 
-// dinero ganará después de un mes si el banco le pagará intereses del 2% 
-// mensual, cree un algoritmo para solucionarlo.
+// dinero ganará después de ciertos dias si el banco le pagará intereses del 15% 
+// anual, cree un algoritmo para solucionarlo.
 
 public class GananciaDeInversion {
 
     public static void earned(int dias, double capital) {
-        double ganancia;
-        ganancia = dias * capital * 0.02;
-        System.out.println("Su ganancia es: " + ganancia + " tras invertir: " + capital + " durante: " + dias + " días.");
+
+        double interesDiario = 0.041095/100, ganancia;
+
+        for (int i = 0; i < dias; i++) {
+            ganancia = capital*interesDiario;
+            capital = capital + ganancia;
+            System.out.println("Capital actual: " + capital + " ganancia diaria: " + ganancia );
+        }
+        System.out.println("El capital Final tras " + dias + " es de: $" + capital);
     }
 
     public static void main(String[] args) {
@@ -24,6 +30,8 @@ public class GananciaDeInversion {
         System.out.println("Ingrese el número de días que el dinero estuvo invertido:");
         dias = scan.nextInt();
 
-        LinearStructure.earned(dias, capital);
+        GananciaDeInversion.earned(dias, capital);
+
+        scan.close();
     }
 }
---
+++ README.md
@@ -0,0 +1,1 @@
+# A set of algorithms for solving various basic problems with java
